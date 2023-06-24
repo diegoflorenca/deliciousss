ad0e6597b9c94c326be416b686321bfdfdbfa70c
diegoflorenca <diegoflorenca@gmail.com>
2023-06-24 14:54:06 -0700

    feat: fetch popular recipes from the API

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,5 +1,32 @@
-function Popular() {
-  return <div>Popular</div>;
-}
+import { useEffect, useState } from "react";
+
+const Popular = () => {
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    // TODO: add try catch block here
+    const api = await fetch(
+      `https://api.edamam.com/api/recipes/v2?type=public&app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_API_KEY}&diet=balanced&random=true`
+    );
+    const data = await api.json();
+    setPopular(data.hits);
+    console.log(data.hits);
+  };
+  return (
+    <div>
+      {popular.map((item) => {
+        return (
+          <div key={item.recipe.uri}>
+            <p>{item.recipe.label}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Popular;
