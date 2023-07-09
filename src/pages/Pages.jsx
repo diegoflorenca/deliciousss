d2185311ff9b7eda0df3b13ed80968749aa7766b
diegoflorenca <diegoflorenca@gmail.com>
2023-07-09 08:00:17 -0700

    feat: add recipe page

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Cuisine from "./Cuisine";
 import SearchResults from "./SearchResults";
+import Recipe from "./Recipe";
 
 function Pages() {
   return (
@@ -9,6 +10,7 @@ function Pages() {
       <Route path="/" element={<Home />} />
       <Route path="/cuisine/:type" element={<Cuisine />} />
       <Route path="/search/:input" element={<SearchResults />} />
+      <Route path="/recipe/:uri" element={<Recipe />} />
     </Routes>
   );
 }
